e5bf3b404b5888f6138e26f0794e5ba86b99fbe2
baraahhawari <baraah.hawari@gmail.com>
2021-06-24 12:11:54 +0300

    done

---
+++ app.js
@@ -0,0 +1,110 @@
+'use strict';
+
+// yesterday review 
+
+// Functions
+// function declaration
+// console.log(getAge(2000));
+
+// function getAge(year) {
+//     return 2021 - year;
+// }
+
+
+// // function expression
+
+// let getCity = function(city) {
+//     console.log('I live in ' + city);
+// }
+// getCity('Amman');
+
+// // anonymous functions
+// // doesn't have a name
+// (function() {
+//     console.log('hello I am a self invoked function');
+// })();
+
+// // SCOPES
+
+// // Global Scope
+// let userName = 'Ahmad Jaber';
+
+// // {local} for function and if {}
+// // for (let i = 0; i < 5; i++) {
+// //     let output = 'hello ' + userName;
+// //     console.log('inside the loop ', output);
+// // }
+
+// // console.log(output);
+
+// // function local scope
+
+// function welcome(userName) {
+//     let msg = 'hello hello';
+//     console.log(msg, 'local scope for the function');
+//     console.log('hello ' + userName);
+// }
+// let msg = 'ya welcome we 100 wardeh';
+// console.log(msg, 'Global scope');
+// welcome('Salsabeel');
+// welcome(userName);
+
+// let Age = 200;
+// if (Age >= 100) {
+//     let Age = '300';
+//     console.log(Age + 'in the If statement');
+//     console.log('Are you Vampire');
+// }
+
+// console.log(Age, 'Global');
+
+
+// nested for loop
+// for (let i = 0; i < 6; i++) {
+//     // console.log(i);
+//     console.log('i', i);
+//     for (let j = 0; j < 2; j++) {
+//         // console.log('j');
+//         console.log('j', j);
+//     }
+// }
+
+
+// nested for loop
+// let colors = ['red', 'green', 'blue', 'yellow'];
+
+// for (let i = 0; i < colors.length; i++) {
+//     console.log(colors[i]);
+//     for (let j = 0; j < 2; j++) {
+
+//     }
+
+// }
+
+
+
+// JEHAD
+
+// Global array
+// let arr = [
+//     [1, 2, 3, 4],
+//     [5, 67, 8, 9],
+//     [2, 5, 9, 8],
+//     [22, 55, 88, 99]
+// ];
+
+// // nested for loop
+// // 4 iteration
+// for (let i = 0; i < 4; i++) {
+//     // 4 iteration
+//     for (let j = 0; j < 4; j++) {
+//         // local array
+//         let theArr = arr[i][j];
+//         console.log(theArr);
+//     }
+// }
+
+
+// pair programming
+// I am the owner of the code ==> navigator who will do the talking part
+// the coder, the driver
